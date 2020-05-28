4596ea27647c20f740de3624b1981dcf9d530272
Andreas Röckert <mr.rockert@gmail.com>
2020-05-28 21:16:26 +0200

    Add function to extract primary and secondary letters

--- lib/providers/fetch_from_web.dart
+++ lib/providers/fetch_from_web.dart
@@ -2,6 +2,10 @@ import 'package:http/http.dart';
 import 'package:html/parser.dart';
 import 'package:html/dom.dart';
 
+void main() async {
+  print(extractLetters(await fetchWords(Client())));
+}
+
 Future<List<String>> fetchWords(Client client) async {
   // var client = Client();
   Response response = await client.get('https://nytbee.com/');
@@ -14,4 +18,43 @@ Future<List<String>> fetchWords(Client client) async {
       links.map((link) => link.text).map((word) => word.trim()).toList();
 
   return words;
+}
+
+Map<String, dynamic> extractLetters(List<String> words) {
+  Map<String, int> letterCount = {};
+
+  void addToMap(letter) {
+    if (!letterCount.containsKey(letter)) letterCount[letter] = 0;
+
+    letterCount[letter] += 1;
+  }
+
+  words.forEach(
+    (word) {
+      word.split('').toSet().forEach(
+        (letter) {
+          addToMap(letter);
+        },
+      );
+    },
+  );
+  // find the primary letter
+
+  var nums = letterCount.values.toList();
+  nums.sort((a, b) => a.compareTo(b));
+
+  var primaryMap = Map<String, int>.from(letterCount);
+  primaryMap.removeWhere((key, value) => value != nums.last);
+  String primaryLetter = primaryMap.keys.toList()[0];
+
+  var secondaryMap = Map<String, int>.from(letterCount);
+  secondaryMap.removeWhere((key, value) => value == nums.last);
+  List<String> secondaryLetter = secondaryMap.keys.toList();
+
+  Map<String, dynamic> letters = {
+    'primaryLetter': primaryLetter,
+    'secondaryLetters': secondaryLetter,
+  };
+
+  return letters;
 }
