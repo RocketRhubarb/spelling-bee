af2cd7470b929cdc553bb95038adde58ef4fa6ae
Andreas Röckert <andreas.rockert@gmail.com>
2021-07-26 22:44:41 +0200

    Add fromMap and toMap methods

--- lib/models/dictionary_model.dart
+++ lib/models/dictionary_model.dart
@@ -4,4 +4,18 @@ class DictionaryModel {
   List<String> secondaryLetters;
 
   DictionaryModel({this.words, this.primaryLetter, this.secondaryLetters});
+
+  static DictionaryModel fromMap(Map<String, dynamic> map) {
+    return DictionaryModel(
+      words: map['words'],
+      primaryLetter: map['primaryLetter'],
+      secondaryLetters: map['secondaryLetters'],
+    );
+  }
+
+  Map toMap() => {
+        'words': words,
+        'primaryLetter': primaryLetter,
+        'secondaryLetters': secondaryLetters,
+      };
 }
